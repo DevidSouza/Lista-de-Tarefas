805ffe27ffff5aa181ec88b3c1f69cf4fc666193
Devid Souza <devid.souza140@gmail.com>
2023-06-06 22:33:51 -0300

    Criação da função 'checked_task_to_down' e mundaça de algumas variáveis

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 let id = 0;
+let todas_as_tarefas = [];
 
 function add_tarefa() {
     let input = String(document.getElementById("input").value);
     let capitaliza = input;
-    if (input !== ''){
-        capitaliza = input[0].toUpperCase() + input.substring(1);   
+    if (input !== '') {
+        capitaliza = input[0].toUpperCase() + input.substring(1);
     };
-    document.getElementById("area-tarefa").innerHTML +=
-        "<div id='item" + id + "' class='tarefa-e-button-del'><input id='btn-checkout" + id + "' class='btn-checkout' name='card' type='radio'><div onclick='executa_complete_task()' id='tarefa" + id + "' class='tarefa'><h4 id='texto" + id + "' class='texto'>" + capitaliza + "</h4></div></div><button onclick='executa_del_tarefa()' id='button" + id + "' class='del-button'><img src='icons/lixeira.png'style='max-width: 15%; color: white;'>Deletar</button>";
-
+    todas_as_tarefas.push("<div id='item" + id + "' class='tarefa-e-button-del'><input id='btn-checkout" + id + "' class='btn-checkout' name='card' type='radio'><div onclick='executa_complete_task()' id='tarefa" + id + "' class='tarefa'><h4 id='texto" + id + "' class='texto'>" + capitaliza + "</h4></div></div><button onclick='executa_del_tarefa()' id='button" + id + "' class='del-button'><img src='icons/lixeira.png'style='max-width: 15%; color: white;'>Deletar</button>");
+    document.getElementById("area-tarefa").innerHTML = ''
+    for (tarefa in todas_as_tarefas) {
+        document.getElementById("area-tarefa").innerHTML += todas_as_tarefas[tarefa]
+    }
     id += 1;
-
 };
 
 // CAPTURA ID DE UMA TAG FILHO E DELETA
 function executa_del_tarefa() {
-    var area_tarefa = document.getElementById('area-tarefa');
+    let area_tarefa = document.getElementById('area-tarefa');
     area_tarefa.onclick = area_tarefa.addEventListener('click', del_tarefa)
 }
 
@@ -32,8 +34,8 @@ function del_tarefa(e) {
     }
 }
 
-function remove_id(tarefa_id) {
-    document.getElementById(tarefa_id).remove();
+function remove_id(id_from_task) {
+    document.getElementById(id_from_task).remove();
 }
 
 function get_number(str) {
@@ -46,7 +48,6 @@ function get_number(str) {
     return number
 }
 
-
 // =============================================================
 
 
@@ -56,30 +57,49 @@ function executa_complete_task() {
 }
 
 function complete_task(e) {
-    let tarefa_id = String(e.target.id);
-    let checked_btn_id = 'btn-checkout' + get_number(tarefa_id);
+    let id_from_task = String(e.target.id);
+    let checked_btn_id = 'btn-checkout' + get_number(id_from_task);
     let item_id = 'item' + get_number(checked_btn_id);
 
-    if (tarefa_id.includes('tarefa')) {
-        task_checked(item_id, checked_btn_id);
+    if (id_from_task.includes('tarefa')) {
+        checked_task(item_id, checked_btn_id);
     }
     else {
         return
     }
 }
 
-function task_checked(tarefa, btn_id) {
-    if (document.getElementById(tarefa).style.backgroundColor === 'rgba(16, 233, 81, 0.808)') {
+function checked_task_to_down(id_item, list) {
+    let task_and_button = ''
+    for (task_index in list) {
+        if (list[task_index].includes(id_item)) {
+            task_and_button = list[task_index]
+            list.splice(task_index, 1)
+            list.push(task_and_button)
+            return list
+        }
+    }
+}
+
+function checked_task(item_id, btn_id) {
+    if (document.getElementById(item_id).style.backgroundColor === 'rgba(16, 233, 81, 0.808)') {
         console.log('Deu certo');
-        document.getElementById(tarefa).style.backgroundColor = 'white';
-        document.getElementById('texto' + get_number(tarefa)).style.textDecoration = 'none'
-        document.getElementById(tarefa).style.opacity = '1';
+        document.getElementById(item_id).style.backgroundColor = 'white';
+        document.getElementById('texto' + get_number(item_id)).style.textDecoration = 'none'
+        document.getElementById(item_id).style.opacity = '1';
         document.getElementById(btn_id).checked = false;
         return
     };
-    document.getElementById(tarefa).style.backgroundColor = 'rgba(16, 233, 81, 0.808)';
-    document.getElementById(tarefa).style.opacity = '0.7';
-    document.getElementById('texto' + get_number(tarefa)).style.textDecoration = 'line-through';
+
+    let task_list = checked_task_to_down(item_id, todas_as_tarefas);
+    document.getElementById("area-tarefa").innerHTML = ''
+    for (task in task_list) {
+        document.getElementById("area-tarefa").innerHTML += task_list[task]
+    }
+
+    document.getElementById(item_id).style.backgroundColor = 'rgba(16, 233, 81, 0.808)';
+    document.getElementById(item_id).style.opacity = '0.7';
+    document.getElementById('texto' + get_number(item_id)).style.textDecoration = 'line-through';
     document.getElementById(btn_id).checked = true;
 }
 
