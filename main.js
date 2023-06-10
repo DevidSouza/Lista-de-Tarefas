99fdaa18919079886adb24986947722a1708fdbb
Devid Souza <devid.souza140@gmail.com>
2023-06-10 20:14:10 -0300

    Adicionando a funcionalidade de mover a tarefa para cima quando desmarcada

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let id = 0;
-let todas_as_tarefas = [];
+var todas_as_tarefas = [];
 
 function add_tarefa() {
     let input = String(document.getElementById("input").value);
@@ -26,8 +26,14 @@ function del_tarefa(e) {
     let item_id = String("item" + get_number(button_id));
 
     if (button_id.includes('button')) {
-        remove_id(item_id);
-        remove_id(button_id);
+        for (task in todas_as_tarefas) {
+            if (todas_as_tarefas[task].includes(item_id)) {
+                remove_id(item_id);
+                remove_id(button_id);
+                delete todas_as_tarefas[task]
+                return
+            }
+        }
     }
     else {
         return
@@ -69,38 +75,68 @@ function complete_task(e) {
     }
 }
 
-function checked_task_to_down(id_item, list) {
-    let task_and_button = ''
+function checked_task_to_down(item, list, button, checked) {
+    let task_and_button = '';
     for (task_index in list) {
-        if (list[task_index].includes(id_item)) {
-            task_and_button = list[task_index]
-            list.splice(task_index, 1)
-            list.push(task_and_button)
-            return list
+        if (list[task_index].includes(String(item.id)) && checked == true) {
+
+            task_and_button = item.outerHTML + button.outerHTML;
+            list.splice(task_index, 1);
+            list.push(String(task_and_button));
+            return list;
+        }
+
+        if (list[task_index].includes(String(item.id)) && checked == false) {
+
+            task_and_button = item.outerHTML + button.outerHTML;
+            list.splice(task_index, 1);
+            list.unshift(String(task_and_button));
+            return list;
         }
     }
 }
 
 function checked_task(item_id, btn_id) {
     if (document.getElementById(item_id).style.backgroundColor === 'rgba(16, 233, 81, 0.808)') {
-        console.log('Deu certo');
         document.getElementById(item_id).style.backgroundColor = 'white';
         document.getElementById('texto' + get_number(item_id)).style.textDecoration = 'none'
         document.getElementById(item_id).style.opacity = '1';
         document.getElementById(btn_id).checked = false;
+
+        let object_item = document.querySelector('#' + item_id);
+        let object_button = document.querySelector('#button' + get_number(btn_id));
+
+
+        // console.log(object_item)
+        // console.log(object_button)
+
+        let task_list = checked_task_to_down(object_item, todas_as_tarefas, object_button, false);
+        document.getElementById("area-tarefa").innerHTML = ''
+        for (task in task_list) {
+            document.getElementById("area-tarefa").innerHTML += task_list[task]
+
+        }
         return
     };
-
-    let task_list = checked_task_to_down(item_id, todas_as_tarefas);
-    document.getElementById("area-tarefa").innerHTML = ''
-    for (task in task_list) {
-        document.getElementById("area-tarefa").innerHTML += task_list[task]
-    }
 
     document.getElementById(item_id).style.backgroundColor = 'rgba(16, 233, 81, 0.808)';
     document.getElementById(item_id).style.opacity = '0.7';
     document.getElementById('texto' + get_number(item_id)).style.textDecoration = 'line-through';
     document.getElementById(btn_id).checked = true;
+
+    let object_item = document.querySelector('#' + item_id);
+    let object_button = document.querySelector('#button' + get_number(btn_id));
+
+
+    // console.log(object_item)
+    // console.log(object_button)
+
+    let task_list = checked_task_to_down(object_item, todas_as_tarefas, object_button, true);
+    document.getElementById("area-tarefa").innerHTML = ''
+    for (task in task_list) {
+        document.getElementById("area-tarefa").innerHTML += task_list[task]
+
+    }
 }
 
 
